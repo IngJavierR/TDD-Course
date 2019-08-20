6e45ef5cd062a752c76593f7deca16cbb5546b31
JAVIER RODRIGUEZ <javierrodriguez@MacBook-Pro-de-JAVIER.local>
2019-08-20 14:02:05 -0500

    First commit

---
+++ TDD-Java-Course/src/test/java/com/example/company/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.company;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ TDD-Net-Course/TDD-Net-Course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD_Net_Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
